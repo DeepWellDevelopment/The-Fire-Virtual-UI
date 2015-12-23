2ea9be3d9903281ec97554a88b8fa5e1bd746f23
cjs07 <cjschaefer1999@gmail.com>
2015-12-23 10:17:14 -0500

    Update Input.java

--- Input.java
+++ Input.java
@@ -1,5 +1,22 @@
 package com.deepwelldevelopment.thefire.core.io
 
+import java.io.File;
+import java.io.Scanner;
+
 public class Input {
+  
+  File source;
+  Scanner scanner;
+  String currentCommand;
+  
+  public Input(File source) {
+    this.source = source;
+    scanner = new Scanner(source);
+  }
+  
+  public void pollInput() {
+    currentCommand = scanner.nextln();
+  }
+  
   
 }
